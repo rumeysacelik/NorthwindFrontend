8b3d079257f4366b3869c14fc92f96c9adb1f015
rumeysacelik <rmysclk61@icloud.com>
2021-03-17 23:07:01 +0300

    x

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,9 +12,10 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
+  filterText="";
   
   constructor(private productService:ProductService, 
-    private activatedRoute:ActivatedRoute) {}
+    private activatedRoute:ActivatedRoute, private toastrService:ToastrService) {}
 
   ngOnInit(): void {
       this.activatedRoute.params.subscribe(params=>{
@@ -37,5 +39,10 @@ export class ProductComponent implements OnInit {
       this.products = response.data
       this.dataLoaded = true;
     })   
+  }
+
+  addToCart(product:Product){
+    this.toastrService.success("Sepete eklendi",product.productName)
+
   }
 }
---
+++ src/app/pipes/filterpipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterPipePipe } from './filterpipe.pipe';
+
+describe('FilterpipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
